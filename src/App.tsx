1139ff5ff7656e366f842ff0185d1556063eeab1
EL MASDOURI Reda <reda.el-masdouri@etu.univ-amu.fr>
2023-10-17 11:45:45 +0200

    add prod and dev envConfig

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,10 @@ import LOGO from './pngwing.svg';
 export const App = () => {
   return (
     <>
-      <h1>React TypeScript Webpack Starter Template</h1>
+      <h1>
+        React TypeScript Webpack Starter Template -{' '}
+        {process.env.NODE_ENV} {process.env.name}
+      </h1>
       <img src={IMAGE} alt="React Logo" width={300} height={300} />
       <img src={LOGO} alt="React Logo" />
     </>
