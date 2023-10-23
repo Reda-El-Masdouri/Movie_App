cf91b4bd9c4500b8aed10c00242f2f25f779d1ac
EL MASDOURI Reda <reda.el-masdouri@etu.univ-amu.fr>
2023-10-23 23:04:45 +0200

    put index.html into public folder. uninstall copypluging. add readme.

---
+++ README.md
@@ -0,0 +1,11 @@
+# Movie_App
+
+## Description
+
+This app provides to visitors a list of movies (photos + titles), they can consult each of them. When consulting, the visitor will be directed to the movie page which contain a lot of information of it like IMDB score, description ...
+
+## Stacks
+
+Webpack, Typescript, Sass, React, React Dom, Redux, Toolkit, Bootstrap.
+
+## Architecture
--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,6 +1,5 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
-const CopyPlugin = require('copy-webpack-plugin')
 
 module.exports = {
   entry: path.resolve(__dirname, '..', './src/index.tsx'),
@@ -58,10 +57,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, '..', './src/index.html'),
-    }),
-    new CopyPlugin({
-      patterns: [{ from: 'source', to: 'dest' }],
+      template: path.resolve(__dirname, '..', './public/index.html'),
     }),
   ],
   stats: 'errors-only',
