538acb18e1300c809a411b32c992f7327d19723e
EL MASDOURI Reda <reda.el-masdouri@etu.univ-amu.fr>
2023-11-01 15:41:57 +0100

    feat: header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,5 +1,16 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
+import user from '../../images/user.png'
+import './Header.scss'
 export default function Header() {
-  return <div>Header</div>
+  return (
+    <div className="header">
+      <Link to="/">
+        <div className="logo">Movie App</div>
+      </Link>
+      <div className="user-img">
+        <img src={user} alt="user" />
+      </div>
+    </div>
+  )
 }
