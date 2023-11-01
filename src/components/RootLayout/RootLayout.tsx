86f8f81d41d740b9815c36241f67d93417f84d31
EL MASDOURI Reda <reda.el-masdouri@etu.univ-amu.fr>
2023-11-01 15:43:54 +0100

    style: RootLayout

--- src/components/RootLayout/RootLayout.tsx
+++ src/components/RootLayout/RootLayout.tsx
@@ -2,19 +2,23 @@ import React from 'react'
 import { NavLink, Outlet } from 'react-router-dom'
 import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
+import './RootLayout.scss'
+
 export default function RootLayout() {
   return (
     <div>
-      <header>
-        <nav>
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/about">About</NavLink>
-        </nav>
-      </header>
       <Header />
-      <main>
-        <Outlet />
-      </main>
+      <div className="container">
+        <header>
+          <nav>
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/about">About</NavLink>
+          </nav>
+        </header>
+        <main>
+          <Outlet />
+        </main>
+      </div>
       <Footer />
     </div>
   )
