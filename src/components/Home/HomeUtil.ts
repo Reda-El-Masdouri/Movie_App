cbb63dad241495cc63959f8037351db3926a7365
EL MASDOURI Reda <reda.el-masdouri@etu.univ-amu.fr>
2023-11-12 20:32:20 +0100

    feat: add error condition

--- src/components/Home/HomeUtil.ts
+++ src/components/Home/HomeUtil.ts
@@ -1,3 +1,5 @@
 export async function homeLoader () {
+    // il faut mettre cette vérification au moment du fetch pour englober les erreurs du fetch aussi
+    // if(!response.ok) {throw Error('Could not find that movie')}
     return null;
 };
