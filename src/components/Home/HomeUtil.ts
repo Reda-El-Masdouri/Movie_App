0059f013d9309127f298921a1ae310a895d1e23e
EL MASDOURI Reda <reda.el-masdouri@etu.univ-amu.fr>
2023-11-18 22:38:13 +0100

    feat: get movie

--- src/components/Home/HomeUtil.ts
+++ src/components/Home/HomeUtil.ts
@@ -1,5 +1,8 @@
+import movieApi from "../../common/api/movieApi";
+import {apiKey, i} from "../../common/api/MovieApiKey";
 export async function homeLoader () {
     // il faut mettre cette vérification au moment du fetch pour englober les erreurs du fetch aussi
     // if(!response.ok) {throw Error('Could not find that movie')}
-    return null;
+    const response = await movieApi.get(`?i=${i}&apiKey=${apiKey}&type=movie`).catch((error: Error)=>{console.log("Error : ", error)})
+    return response;
 };
