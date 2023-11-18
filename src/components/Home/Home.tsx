52a93427c99d4430e2abda817e7ed482a87c42be
EL MASDOURI Reda <reda.el-masdouri@etu.univ-amu.fr>
2023-11-18 22:38:54 +0100

    add: MovieList component to home

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,7 +1,14 @@
 import React from 'react'
 import { useLoaderData } from 'react-router-dom'
+import MovieList from '../MovieList/MovieList'
 
 export default function Home() {
   const movies = useLoaderData()
-  return <div>Home</div>
+  return (
+    <>
+      <div className="banner-img"></div>
+      <MovieList />
+      {console.log('movies', movies)}
+    </>
+  )
 }
