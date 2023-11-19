8a796f0686bc30289406716953b7792582ce9fe6
EL MASDOURI Reda <reda.el-masdouri@etu.univ-amu.fr>
2023-11-19 21:40:56 +0100

    feat: dispatch addMovies action

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,14 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useLoaderData } from 'react-router-dom'
 import MovieList from '../MovieList/MovieList'
+import { addMovies } from '../../features/movies/movieSlice'
+import { useDispatch } from 'react-redux'
 
 export default function Home() {
   const movies = useLoaderData()
+  const dispatch = useDispatch()
+  useEffect(() => {
+    dispatch(addMovies(movies.data))
+  })
   return (
     <>
       <div className="banner-img"></div>
       <MovieList />
-      {console.log('movies', movies)}
     </>
   )
 }
