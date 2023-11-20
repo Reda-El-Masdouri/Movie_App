8d0423ea3252ad6de5a3a0fea576068b4f1f50e6
EL MASDOURI Reda <reda.el-masdouri@etu.univ-amu.fr>
2023-11-20 22:16:41 +0100

    feat: dispatch actions after home rendering

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,14 +1,17 @@
 import React, { useEffect } from 'react'
 import { useLoaderData } from 'react-router-dom'
 import MovieList from '../MovieList/MovieList'
-import { addMovies } from '../../features/movies/movieSlice'
+import { addMovies, errorMovies } from '../../features/movies/movieSlice'
 import { useDispatch } from 'react-redux'
 
 export default function Home() {
   const movies = useLoaderData()
   const dispatch = useDispatch()
+
   useEffect(() => {
-    dispatch(addMovies(movies.data))
+    if (movies?.data?.Response === 'True')
+      dispatch(addMovies(movies?.data?.Search))
+    if (movies?.code === 'ERR_BAD_REQUEST') dispatch(errorMovies)
   })
   return (
     <>
