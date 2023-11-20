84b3e8c51a1a833adef95b31bbbf9a7284344d67
EL MASDOURI Reda <reda.el-masdouri@etu.univ-amu.fr>
2023-11-20 22:14:25 +0100

    feat: fetch all movies from the state into MovieList component

--- src/components/MovieList/MovieList.tsx
+++ src/components/MovieList/MovieList.tsx
@@ -1,5 +1,31 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { getMovies } from '../../features/movies/movieSlice'
+import { MovieData, MovieSearch } from '../../features/movies'
+import MovieCard from '../MovieCard/MovieCard'
 
 export default function MovieList() {
-  return <div>MovieList</div>
+  const movies: MovieData | MovieSearch | undefined = useSelector(getMovies)
+
+  return movies ? (
+    <>
+      <div className="movie-wrapper">
+        <div className="movie-list">
+          {movies.length > 0 ? (
+            movies.map((movie: MovieSearch, index: number) => (
+              <MovieCard key={index} data={movie} />
+            ))
+          ) : (
+            <h2>Aucun Resultat</h2>
+          )}
+        </div>
+      </div>
+    </>
+  ) : (
+    <>
+      <div className="movies-error">
+        <h3>{movies.Error}</h3>
+      </div>
+    </>
+  )
 }
