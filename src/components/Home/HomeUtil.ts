0c56ff54fd601cc045979c7618cc6d4fc37bed16
EL MASDOURI Reda <reda.el-masdouri@etu.univ-amu.fr>
2023-11-20 22:15:26 +0100

    feat: handle error fetch

--- src/components/Home/HomeUtil.ts
+++ src/components/Home/HomeUtil.ts
@@ -3,6 +3,10 @@ import {apiKey, i} from "../../common/api/MovieApiKey";
 export async function homeLoader () {
     // il faut mettre cette vérification au moment du fetch pour englober les erreurs du fetch aussi
     // if(!response.ok) {throw Error('Could not find that movie')}
-    const response = await movieApi.get(`?i=${i}&apiKey=${apiKey}&type=movie`).catch((error: Error)=>{console.log("Error : ", error)})
+    const response = await movieApi.get(`?i=${i}&apiKey=${apiKey}&s=Harry`).catch((error: Error)=>{
+        console.log("Error : ", error)
+        return error;
+    })
+
     return response;
 };
