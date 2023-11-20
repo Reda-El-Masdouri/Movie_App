a9e60b21da887c0e94b71640a0d961a35ffb271e
EL MASDOURI Reda <reda.el-masdouri@etu.univ-amu.fr>
2023-11-20 22:14:50 +0100

    feat: add props to MovieCard component

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -1,5 +1,10 @@
 import React from 'react'
+import { MovieData, MovieSearch } from '../../features/movies'
 
-export default function MovieCard() {
+export interface MovieCardProps {
+  data: MovieData | MovieSearch | undefined
+}
+
+export default function MovieCard({ data }: MovieCardProps) {
   return <div>MovieCard</div>
 }
