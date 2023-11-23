c92f39d4519bf19587431febbd7d5b7d7238266a
EL MASDOURI Reda <reda.el-masdouri@etu.univ-amu.fr>
2023-11-23 20:57:17 +0100

    feat: implement error msg

--- src/components/MovieList/MovieList.tsx
+++ src/components/MovieList/MovieList.tsx
@@ -3,28 +3,32 @@ import { useSelector } from 'react-redux'
 import { getMovies } from '../../features/movies/movieSlice'
 import { MovieData, MovieSearch } from '../../features/movies'
 import MovieCard from '../MovieCard/MovieCard'
+import './MovieList.scss'
 
 export default function MovieList() {
-  const movies: MovieData | MovieSearch | undefined = useSelector(getMovies)
+  const movies: MovieData[] | MovieSearch[] | undefined = useSelector(getMovies)
 
   return movies ? (
     <>
       <div className="movie-wrapper">
         <div className="movie-list">
-          {movies.length > 0 ? (
-            movies.map((movie: MovieSearch, index: number) => (
-              <MovieCard key={index} data={movie} />
-            ))
-          ) : (
-            <h2>Aucun Resultat</h2>
-          )}
+          <h2>Movies :</h2>
+          <div className="movie-container">
+            {movies.length > 0 ? (
+              movies.map((movie: MovieSearch, index: number) => (
+                <MovieCard key={index} data={movie} />
+              ))
+            ) : (
+              <h2>Aucun Resultat</h2>
+            )}
+          </div>
         </div>
       </div>
     </>
   ) : (
     <>
       <div className="movies-error">
-        <h3>{movies.Error}</h3>
+        <h3>Une erreur est survenu ...</h3>
       </div>
     </>
   )
