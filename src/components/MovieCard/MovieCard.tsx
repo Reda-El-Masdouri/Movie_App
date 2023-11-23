fca30e6f85393e91a44e9b019513da526269daea
EL MASDOURI Reda <reda.el-masdouri@etu.univ-amu.fr>
2023-11-23 20:57:55 +0100

    feat: implement MovieCard component with data

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import { MovieData, MovieSearch } from '../../features/movies'
+import './MovieCard.scss'
 
 export interface MovieCardProps {
   data: MovieData | MovieSearch | undefined
 }
 
 export default function MovieCard({ data }: MovieCardProps) {
-  return <div>MovieCard</div>
+  return (
+    <div className="card-item">
+      <div className="card-inner">
+        <div className="card-top">
+          <img src={data?.Poster} alt={data?.Title} />
+        </div>
+        <div className="card-corp">
+          <div className="card-info">
+            <h3>{data?.Title}</h3>
+            <p>{data?.Year}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
