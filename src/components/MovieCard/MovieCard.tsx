086e23866de70a9d1d127a15298e4172cbfd1492
EL MASDOURI Reda <reda.el-masdouri@etu.univ-amu.fr>
2023-11-26 22:08:37 +0100

    feat: add Link and style to MovieCard

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { MovieData, MovieSearch } from '../../features/movies'
 import './MovieCard.scss'
+import { Link } from 'react-router-dom'
 
 export interface MovieCardProps {
   data: MovieData | MovieSearch | undefined
@@ -9,17 +10,19 @@ export interface MovieCardProps {
 export default function MovieCard({ data }: MovieCardProps) {
   return (
     <div className="card-item">
-      <div className="card-inner">
-        <div className="card-top">
-          <img src={data?.Poster} alt={data?.Title} />
-        </div>
-        <div className="card-corp">
-          <div className="card-info">
-            <h3>{data?.Title}</h3>
-            <p>{data?.Year}</p>
+      <Link to={`/movie/${data?.imdbID}`}>
+        <div className="card-inner">
+          <div className="card-top">
+            <img src={data?.Poster} alt={data?.Title} />
+          </div>
+          <div className="card-corp">
+            <div className="card-info">
+              <h3>{data?.Title}</h3>
+              <p>{data?.Year}</p>
+            </div>
           </div>
         </div>
-      </div>
+      </Link>
     </div>
   )
 }
