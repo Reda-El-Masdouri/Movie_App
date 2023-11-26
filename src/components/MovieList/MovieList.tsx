daef2abead1d8c021cd8c1cd2e1a27ea5d771692
EL MASDOURI Reda <reda.el-masdouri@etu.univ-amu.fr>
2023-11-26 22:10:40 +0100

    refacto: list of movies and shows

--- src/components/MovieList/MovieList.tsx
+++ src/components/MovieList/MovieList.tsx
@@ -2,34 +2,20 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import { getMovies } from '../../features/movies/movieSlice'
 import { MovieData, MovieSearch } from '../../features/movies'
-import MovieCard from '../MovieCard/MovieCard'
 import './MovieList.scss'
+import { getShows } from '../../features/shows/showSlice'
+import List from '../List/List'
 
 export default function MovieList() {
   const movies: MovieData[] | MovieSearch[] | undefined = useSelector(getMovies)
+  const shows = useSelector(getShows)
+  console.log('movies', movies)
+  console.log('shows', shows)
 
-  return movies ? (
+  return (
     <>
-      <div className="movie-wrapper">
-        <div className="movie-list">
-          <h2>Movies :</h2>
-          <div className="movie-container">
-            {movies.length > 0 ? (
-              movies.map((movie: MovieSearch, index: number) => (
-                <MovieCard key={index} data={movie} />
-              ))
-            ) : (
-              <h2>Aucun Resultat</h2>
-            )}
-          </div>
-        </div>
-      </div>
-    </>
-  ) : (
-    <>
-      <div className="movies-error">
-        <h3>Une erreur est survenu ...</h3>
-      </div>
+      <List title="Movies" data={movies} />
+      <List title="Shows" data={shows.Search} />
     </>
   )
 }
