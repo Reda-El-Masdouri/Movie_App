87bc03325e9ed18bee257394c4d910f36d55c2bd
EL MASDOURI Reda <reda.el-masdouri@etu.univ-amu.fr>
2023-11-26 22:09:11 +0100

    fix: remove id

--- src/components/Home/HomeUtil.ts
+++ src/components/Home/HomeUtil.ts
@@ -1,9 +1,9 @@
 import movieApi from "../../common/api/movieApi";
-import {apiKey, i} from "../../common/api/MovieApiKey";
+import {apiKey} from "../../common/api/MovieApiKey";
 export async function homeLoader () {
     // il faut mettre cette vérification au moment du fetch pour englober les erreurs du fetch aussi
     // if(!response.ok) {throw Error('Could not find that movie')}
-    const response = await movieApi.get(`?i=${i}&apiKey=${apiKey}&s=Harry`).catch((error: Error)=>{
+    const response = await movieApi.get(`?apiKey=${apiKey}&s=Harry`).catch((error: Error)=>{
         console.log("Error : ", error)
         return error;
     })    
