351bc9b43e32c0a463202d833751fba3b67c3349
EL MASDOURI Reda <reda.el-masdouri@etu.univ-amu.fr>
2023-12-02 22:10:32 +0100

    fix: remove useless lines

--- src/components/MovieList/MovieList.tsx
+++ src/components/MovieList/MovieList.tsx
@@ -2,15 +2,13 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import { getMovies } from '../../features/movies/movieSlice'
 import { MovieData, MovieSearch } from '../../features/movies'
-import './MovieList.scss'
 import { getShows } from '../../features/shows/showSlice'
 import List from '../List/List'
+import './MovieList.scss'
 
 export default function MovieList() {
   const movies: MovieData[] | MovieSearch[] | undefined = useSelector(getMovies)
   const shows = useSelector(getShows)
-  console.log('movies', movies)
-  console.log('shows', shows)
 
   return (
     <>
