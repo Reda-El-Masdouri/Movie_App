db435137afe0693ea5aaef62db7d1bf4de3dddad
EL MASDOURI Reda <reda.el-masdouri@etu.univ-amu.fr>
2023-12-02 22:48:36 +0100

    feat: add search bar to Header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import user from '../../images/user.png'
 import './Header.scss'
 export default function Header() {
+  const [text, setText] = useState('')
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(text)
+  }
   return (
     <div className="header">
-      <Link to="/">
-        <div className="logo">Movie App</div>
-      </Link>
+      <div className="logo">
+        <Link to="/">Movie App</Link>
+      </div>
+      <div className="search-bar">
+        <form onSubmit={handleSubmit} className="search-form">
+          <input
+            type="text"
+            placeholder="Search movies or shows"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+          <button type="submit">
+            <i className="fa fa-search"></i>
+          </button>
+        </form>
+      </div>
       <div className="user-img">
         <img src={user} alt="user" />
       </div>
