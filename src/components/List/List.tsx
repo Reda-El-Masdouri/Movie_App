cba3ae13d4966cfbccfdfe3f5fa05312684781f0
EL MASDOURI Reda <reda.el-masdouri@etu.univ-amu.fr>
2023-12-04 21:40:43 +0100

    feat: 404 search case

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -5,26 +5,30 @@ import { ListProps } from './ListProps'
 import { MovieData, MovieSearch, showSearch } from '../../features/movies'
 import MyCarousel from '../MyCarousel/MyCarousel'
 export default function List({ title, data }: ListProps) {
-  return data ? (
+  return (
     <>
-      <div className="data-wrapper">
-        <div className="data-list">
-          <h2>{title} :</h2>
-          <div className="data-container">
-            {data.length > 0 ? (
-              <MyCarousel data={data}></MyCarousel>
-            ) : (
-              <h2>Aucun Resultat</h2>
-            )}
+      <h2 className="title-list">{title} :</h2>
+      {data ? (
+        <>
+          <div className="data-wrapper">
+            <div className="data-list">
+              <div className="data-container">
+                {data.length > 0 ? (
+                  <MyCarousel data={data}></MyCarousel>
+                ) : (
+                  <h2>Aucun Resultat</h2>
+                )}
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-    </>
-  ) : (
-    <>
-      <div className="data-error">
-        <h3>Une erreur est survenu ...</h3>
-      </div>
+        </>
+      ) : (
+        <>
+          <div className="data-error">
+            <h3>Une erreur est survenu ...</h3>
+          </div>
+        </>
+      )}
     </>
   )
 }
